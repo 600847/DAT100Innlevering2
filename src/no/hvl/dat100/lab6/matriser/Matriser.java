be7121e780ad6d4927fbd98b02a14500f5e37b71
600847 <august.helle1@gmail.com>
2021-09-28 16:51:41 +0200

    O1-B Alle obligatoriske oppgaver gjort

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -3,34 +3,74 @@ package no.hvl.dat100.lab6.matriser;
 public class Matriser {
 
 	// a)
-	public static void skrivUt(int[][] matrise) {
+	public static void skrivUt(int[][] a) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-		//test
+		for(int []rad: a) {
+			for (int tall: rad) {
+			System.out.print(tall);
+			}
+			System.out.println("");
+		}
+		
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String tekst = "";
+		for(int []rad:matrise) {
+			for(int tall: rad) {
+			tekst += tall + " ";
+			}
+			tekst += '\n';
+			
+		}
+		return tekst;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		int[][] nyTabell = new int[matrise.length][];
+		for(int i = 0; i < matrise.length; i++) {
+			nyTabell[i] = new int[matrise[i].length];
+		
+		}
+		
+		for(int i = 0; i < matrise.length; i++)
+			for(int j= 0; j < matrise[i].length; j++) {
+				nyTabell[i][j] = matrise[i][j]*tall;
+				
+				
+			}
+		return nyTabell;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		boolean likAntallRad = false;
+		boolean likTall = false;
+		boolean erLik = false;
+	for(int i= 0; i<a.length; i++) {
+			if (a[i].length == b[i].length) 
+				likAntallRad = true;
+			else 
+				likAntallRad = false;
+			
+			for(int j= 0; j < a[i].length; j++) {
+				if (a[i][j] == b[i][j])
+					likTall = true;
+				else
+					likTall = false;
+			}	
+		}
+	
+		if (likAntallRad && likTall) {
+		erLik = true;
+		}
+		
+		return erLik;
+		
 	}
 	
 	// e)
